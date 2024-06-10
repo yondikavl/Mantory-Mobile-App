cf869328fff85bf61f2037225583cff7d2035cc9
yondikavl <yondikaviolanda@gmail.com>
2024-06-10 19:22:29 +0700

    fix: data catalog (load from database)

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,163 +1,180 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:qrcode_app/app/data/models/product_model.dart';
 
 class HomeController extends GetxController {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  RxList<ProductModel> allProducts = List<ProductModel>.empty().obs;
+
   void downloadCatalog() async {
     final pdf = pw.Document();
 
+    var getData = await firestore.collection("products").get();
+
+    // reset all product (mengatasi duplikat)
+    allProducts([]);
+    // data dari database
+    for (var element in getData.docs) {
+      allProducts.add(ProductModel.fromJson(element.data()));
+    }
+
     pdf.addPage(
-      pw.Page(
-          pageFormat: PdfPageFormat.a4,
-          build: (context) {
-            return pw.Column(
-              children: [
-                pw.Center(
-                  child: pw.Text(
-                    "Catalog Products",
-                    textAlign: pw.TextAlign.center,
-                    style: const pw.TextStyle(
-                      fontSize: 24,
+      pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        build: (context) {
+          List<pw.TableRow> allData = List.generate(
+            allProducts.length,
+            (index) {
+              ProductModel product = allProducts[index];
+              return pw.TableRow(
+                children: [
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      "${index + 1}",
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
                     ),
                   ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      product.code,
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      product.name,
+                      textAlign: pw.TextAlign.center,
+                      style: const pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.Text(
+                      "${product.qty}",
+                      textAlign: const pw.TextAlign.center,
+                      style: pw.TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(20),
+                    child: pw.BarcodeWidget(
+                      color: PdfColor.fromHex("#000000"),
+                      barcode: pw.Barcode.qrCode(),
+                      data: product.code,
+                      height: 50,
+                      width: 50,
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+
+          return [
+            pw.Center(
+              child: pw.Text(
+                "Catalog Products",
+                textAlign: pw.TextAlign.center,
+                style: const pw.TextStyle(
+                  fontSize: 24,
                 ),
-                pw.SizedBox(height: 20),
-                pw.Table(
-                  border: pw.TableBorder.all(
-                    color: PdfColor.fromHex("#000000"),
-                    width: 2,
-                  ),
+              ),
+            ),
+            pw.SizedBox(height: 20),
+            pw.Table(
+              border: pw.TableBorder.all(
+                color: PdfColor.fromHex("#000000"),
+                width: 2,
+              ),
+              children: [
+                // HEAD
+                pw.TableRow(
                   children: [
-                    // HEAD
-                    pw.TableRow(
-                      children: [
-                        pw.Padding(
-                          padding: const pw.EdgeInsets.all(20),
-                          child: pw.Text(
-                            "No",
-                            textAlign: pw.TextAlign.center,
-                            style: pw.TextStyle(
-                              fontSize: 12,
-                              fontWeight: pw.FontWeight.bold,
-                            ),
-                          ),
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "No",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
                         ),
-                        pw.Padding(
-                          padding: const pw.EdgeInsets.all(20),
-                          child: pw.Text(
-                            "Product Code",
-                            textAlign: pw.TextAlign.center,
-                            style: pw.TextStyle(
-                              fontSize: 12,
-                              fontWeight: pw.FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        pw.Padding(
-                          padding: const pw.EdgeInsets.all(20),
-                          child: pw.Text(
-                            "Product Name",
-                            textAlign: pw.TextAlign.center,
-                            style: pw.TextStyle(
-                              fontSize: 12,
-                              fontWeight: pw.FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        pw.Padding(
-                          padding: const pw.EdgeInsets.all(20),
-                          child: pw.Text(
-                            "Quantity",
-                            textAlign: pw.TextAlign.center,
-                            style: pw.TextStyle(
-                              fontSize: 12,
-                              fontWeight: pw.FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        pw.Padding(
-                          padding: const pw.EdgeInsets.all(20),
-                          child: pw.Text(
-                            "QR Code",
-                            textAlign: pw.TextAlign.center,
-                            style: pw.TextStyle(
-                              fontSize: 12,
-                              fontWeight: pw.FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                    // ISI DATA
-                    ...List.generate(
-                      5,
-                      (index) {
-                        return pw.TableRow(
-                          children: [
-                            pw.Padding(
-                              padding: const pw.EdgeInsets.all(20),
-                              child: pw.Text(
-                                "${index + 1}",
-                                textAlign: pw.TextAlign.center,
-                                style: pw.TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                            pw.Padding(
-                              padding: const pw.EdgeInsets.all(20),
-                              child: pw.Text(
-                                "123456789",
-                                textAlign: pw.TextAlign.center,
-                                style: pw.TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                            pw.Padding(
-                              padding: const pw.EdgeInsets.all(20),
-                              child: pw.Text(
-                                "Product 1",
-                                textAlign: pw.TextAlign.center,
-                                style: pw.TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                            pw.Padding(
-                              padding: const pw.EdgeInsets.all(20),
-                              child: pw.Text(
-                                "${index + 25324}",
-                                textAlign: pw.TextAlign.center,
-                                style: pw.TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                            pw.Padding(
-                              padding: const pw.EdgeInsets.all(20),
-                              child: pw.Text(
-                                "QR Code",
-                                textAlign: pw.TextAlign.center,
-                                style: pw.TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "Product Code",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "Product Name",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "Quantity",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    pw.Padding(
+                      padding: const pw.EdgeInsets.all(20),
+                      child: pw.Text(
+                        "QR Code",
+                        textAlign: pw.TextAlign.center,
+                        style: pw.TextStyle(
+                          fontSize: 10,
+                          fontWeight: pw.FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ],
                 ),
+                // ISI DATA
+                ...allData,
               ],
-            );
-          }),
+            ),
+          ];
+        },
+      ),
     );
 
     // simpan
